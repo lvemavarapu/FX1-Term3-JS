35a068a593ec6ec60ce33d2b48379684868a03e8
lvemavarapu <lvemavarapu@gmail.com>
2021-08-23 20:25:24 +1000

    restAndSpread

---
+++ restSpread.js
@@ -0,0 +1,20 @@
+let petAnimals = ["cat","dog","rabbit"]
+let wildAnimals =["Lion","Tiger","Bear"]
+
+let animals =[...petAnimals, ...wildAnimals]
+console.log(animals)
+
+let people = ["Lavanya", "Shauna", "Teej"];
+let [firstPerson] = people;
+console.log(firstPerson);
+
+// spread operator.
+let numbers = [1, 2, 3, 4];
+let [first, second, ...others] = numbers;
+console.log(numbers)
+
+let newNumbers = [34,56,...others]
+console.log(newNumbers)
+
+let favColors = {sally: "blue", tom: "red", scott: "purple"};
+let { first } = favColors;
