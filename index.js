e3fdd04d4bd1a2f699a3d6777029958426bc5199
lvemavarapu <lvemavarapu@gmail.com>
2021-08-26 21:19:14 +1000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-const Joke = require('awesome-dev-jokes');
-const {dividedBy}= require('./src/divide')
- 
-console.log(Joke.getRandomJoke());
-
-console.log("let us learn Node")
-console.log(dividedBy(20, 0))
